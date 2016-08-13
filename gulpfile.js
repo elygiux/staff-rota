3561c4b029d0a7b142ae8985896b520b0fa46b9b
Eligijus <eligijuslipskis@gmail.com>
2016-08-13 16:25:45 +0300

    Creating bower components in resources/vendor. Adding gitignore files to css and js in public and in resources css. Using less, instead of sass.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,16 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function (mix) {
+    mix.less('app.less', 'resources/assets/css/app.css');
+
+    mix
+        .styles([
+            'css/app.css',
+            'vendor/bootstrap/dist/css/bootstrap.min.css'
+        ], 'public/css/landing.min.css', 'resources/assets')
+        .scripts([
+            'jquery/dist/jquery.min.js',
+            'bootstrap/dist/js/bootstrap.min.js'
+        ], 'public/js/app.min.js', 'resources/assets/vendor');
 });
